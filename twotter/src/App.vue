36e141526f1181fe1ac185a1a8e8a4717c756a75
sheepdog <sheepdog@MacBook-Air-sheepdog.local>
2023-07-28 17:11:05 +0200

    added router, css and some data

--- twotter/src/App.vue
+++ twotter/src/App.vue
@@ -1,3 +1,63 @@
 <template>
-  <h2>twotter</h2>
+  <div id="app">
+    <nav>
+      <RouterLink to="/">
+        <div class="navigation__logo">Twooter</div>
+      </RouterLink>
+      <div class="navigation__user">
+        {{ user.username }}
+      </div>
+    </nav>
+    <router-view></router-view>
+  </div>
 </template>
+
+<script>
+import { RouterLink } from "vue-router";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      user: {
+        username: "Jan Kowalski",
+      },
+    };
+  },
+  components: { RouterLink },
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  text-decoration: none;
+}
+
+#app {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  min-height: 100vh;
+  background-color: #f3f5fa;
+
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 5%;
+    background-color: deeppink;
+    color: white;
+
+    .navigation__logo {
+      font-weight: bold;
+      font-size: 24px;
+      color: white;
+    }
+
+    .navigation__user {
+      font-weight: bold;
+    }
+  }
+}
+</style>
---
+++ twotter/src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div class="users-list">
+      <RouterLink
+        v-for="user in users"
+        :to="{ name: 'UserProfile', params: { userId: user.id } }"
+        :key="user.id"
+      >
+        {{ user.username }}
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<script>
+import { RouterLink } from "vue-router";
+import { users } from "../assets/users";
+
+export default {
+  name: "Home",
+  setup() {
+    return {
+      users,
+    };
+  },
+  components: { RouterLink },
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  padding: 50px 5%;
+}
+.users-list {
+  display: flex;
+  flex-direction: column;
+}
+</style>
